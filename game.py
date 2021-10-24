a3d5d0dcd22d499d2bcd381d2cfc678719047f79
Drakonkinst <drakonkinstmc@gmail.com>
2021-10-23 22:24:12 -0700

    Add features
    - Apology in advance for the length and vagueness of this commit
    - Changed how folded players are tracked
    - Added forced Sabaac pot ante
    - Implemented all-in mechanics
    - Add printCurrentGameState helper function
    - Implemented ties and pure Sabaac
    - Some other bugfixes

--- game.py
+++ game.py
@@ -3,8 +3,11 @@ from deck import Deck
 from player import Player
 
 class Game:
-    STARTING_HAND_SIZE = 2
+    STARTING_HAND_SIZE = 5
+    SABAAC_POT_ANTE = 2
     ANTE = 2
+    SABAAC_VALUE = 0
+    BOMB_OUT = 24
     
     @staticmethod
     def printMenu(options):
@@ -33,17 +36,31 @@ class Game:
         #pass
     
     def __init__(self, playerNameList, startingChips = 30):
-        self.deck = Deck.createDeck()
-        self.deck.shuffle()
-        
         self.playerList = []
-        self.foldedPlayers = []
+        self.currentPlayers = []
         for playerName in playerNameList:
             player = Player(playerName)
             player.changeChips(startingChips)
             self.playerList.append(player)
-            
+        
+        self.deck = None
         self.handPot = 0
+        self.sabaacPot = 0
+    
+    def resetRound(self):
+        # Reset current player list
+        self.currentPlayers = [ player for player in self.playerList ]
+        
+        # Empty all hands
+        for player in self.playerList:
+            player.emptyHand()
+
+        # Reset hand pot
+        self.handPot = 0
+        
+        # Reset deck
+        self.deck = Deck.createDeck()
+        self.deck.shuffle()
     
     def playGame(self):
         while len(self.playerList) > 1:
@@ -51,9 +68,12 @@ class Game:
         # Celebrate the last survivor of the apocalypse
     
     def doRound(self):
-        self.foldedPlayers = []
-        self.handPot = 0
+        self.doSabaacPhase()
         
+        if len(self.playerList) <= 1:
+            return
+        
+        self.resetRound()
         self.dealCards()
         self.doBettingPhase(True)
         self.doShiftingPhase()
@@ -71,35 +91,56 @@ class Game:
                 # deal a card to the player
                 self.drawCardForPlayer(player)
     
+    def doSabaacPhase(self):
+        allPlayers = [ player for player in self.playerList ]
+        for player in allPlayers:
+            if player.getChips() <= Game.SABAAC_POT_ANTE:
+                if player.getChips() > 0:
+                    print("Sorry,", player.getName() + ". The House always wins :)")
+                # You're out
+                self.playerList.remove(player)
+            else:
+                player.changeChips(-Game.SABAAC_POT_ANTE)
+                self.sabaacPot += Game.SABAAC_POT_ANTE
+            
     def doBettingPhase(self, withAnte):
+        if len(self.currentPlayers) <= 1:
+            return
+        
         print("BETTING PHASE")
         
         # remainingPlayers = copy of self.playerList
-        remainingPlayers = [ player for player in self.playerList ]
+        remainingPlayers = [ player for player in self.currentPlayers ]
         minCost = 0
         
         if withAnte:
             minCost = Game.ANTE
             
         totalPaidPerPlayer = {}
-        for player in self.playerList:
+        for player in self.currentPlayers:
             totalPaidPerPlayer[player.getName()] = 0
+            
+        # Skip if there's 1 or less players with the ability to bet
+        numWithChips = 0
+        for player in self.currentPlayers:
+            if player.getChips() > 0:
+                numWithChips += 1
         
-        # TODO: Player wins hand if all other players fold
-        # TODO: If first player all-ins
+        if numWithChips <= 1:
+            return
+        
+        # We can assume if chips are 0, they are not out, just broke
         while len(remainingPlayers) > 0:
             # get player from front of queue
             player = remainingPlayers.pop(0)
             
-            if player in self.foldedPlayers:
+            if player.getChips() <= 0:
                 continue
             
             # display the player's hand
-            print(player.getName())
-            print("Chips:", player.getChips())
-            print("Hand Pot:", self.handPot)
-            print("Need to Pay:", minCost - totalPaidPerPlayer[player.getName()])
-            player.printHand()
+            self.printCurrentGameState(player)
+            amountNeeded = minCost - totalPaidPerPlayer[player.getName()]
+            print("Need to Pay:", amountNeeded)
             
             actions = [ "Fold", "Call" ]
             
@@ -107,20 +148,25 @@ class Game:
             
             if minCost == 0:
                 actions[1] = "Check"
-            if player.getChips() > 1:
+            elif amountNeeded >= player.getChips():
+                actions[1] = "All-in"
+            if amountNeeded < player.getChips():
                 actions.append("Raise")
             
             choice = Game.printMenu(actions)
             
             if choice == 1:
-                self.foldedPlayers.append(player)
+                # Fold
+                self.currentPlayers.remove(player)
+                
+                if len(self.currentPlayers) <= 1:
+                    return
             if choice == 2:
                 if minCost == 0:
                     continue
                 
-                amountNeeded = minCost - totalPaidPerPlayer[player.getName()]
-                
                 if amountNeeded > player.getChips():
+                    # All-in
                     amountNeeded = player.getChips()
                 
                 player.changeChips(-amountNeeded)
@@ -138,39 +184,45 @@ class Game:
                 minCost += amountToRaise
                 
                 # Reset the queue starting with the player afterward
-                playerIndex = self.playerList.index(player)
+                playerIndex = self.currentPlayers.index(player)
                 
                 # [p1, p2, p3, p4]
                 remainingPlayers = []
                 
-                for other in self.playerList[(playerIndex + 1):]:
-                    if other in self.foldedPlayers:
-                        continue
+                for other in self.currentPlayers[(playerIndex + 1):]:
                     remainingPlayers.append(other)
                 
-                for other in self.playerList[:playerIndex]:
-                    if other in self.foldedPlayers:
-                        continue
+                for other in self.currentPlayers[:playerIndex]:
                     remainingPlayers.append(other)
     
     def doShiftingPhase(self):
+        if len(self.currentPlayers) <= 1:
+            return
         print("SHIFTING PHASE")
         pass
     
     def doDrawingPhase(self):
+        if len(self.currentPlayers) <= 1:
+            return
         print("DRAW PHASE")
-        for player in self.playerList:
-            print(player.getName())
-            player.printHand()
+        for player in self.currentPlayers:
+            self.printCurrentGameState(player)
             choice = Game.printMenu(["Draw", "Exchange", "Discard", "Skip"])
+            handChanged = False
             
+            # TODO: Idiot's Array trumps normal Sabaac
             if choice == 1:
                 self.drawCardForPlayer(player)
+                handChanged = True
             if choice == 2:
                 # exchange
                 cardChoice = Game.printMenu(player.getHand())
                 discardedCard = player.removeCardAtHandIndex(cardChoice - 1)
                 self.drawCardForPlayer(player)
+                handChanged = True
+                
+                self.deck.addCard(discardedCard)
+                self.deck.shuffle()
             if choice == 3:
                 # discard
                 cardChoice = Game.printMenu(player.getHand())
@@ -178,32 +230,58 @@ class Game:
                 pass
             
             player.printHand()
+            if handChanged:
+                input("Press Enter to continue...")
+            
         pass
     
     def resolveRound(self):
         print("RESOLVE PHASE")
         
         maxMagnitude = -1
-        winningPlayer = None
+        winningPlayers = []
         
-        for player in self.playerList:
+        for player in self.currentPlayers:
             handValue = 0
             for card in player.getHand():
                 handValue += card.getValue()
             handValue = abs(handValue)
             
+            if handValue >= Game.BOMB_OUT:
+                continue
+            
+            if handValue == Game.SABAAC_VALUE:
+                handValue = 999
+            
             if handValue > maxMagnitude:
                 maxMagnitude = handValue
-                winningPlayer = player
+                winningPlayers = []
+            if handValue >= maxMagnitude:
+                winningPlayers.append(player)
         
-        print(winningPlayer.getName(), "wins!")
-        # calculate hand values and any combos of each player, determine winner
-        # and then give them credits
+        if len(winningPlayers) != 1:
+            # Tie
+            if maxMagnitude == Game.SABAAC_VALUE:
+                print("Lmao it's a tie, no one wins")
+            else:
+                print("It's a tie!", self.handPot, "chips go to the Sabaac pot")
+            self.sabaacPot += self.handPot
+        else:
+            winningPlayer = winningPlayers[0]
+            winningPlayer.changeChips(self.handPot)
+            
+            if maxMagnitude == 999:
+                print(winningPlayer.gameName(), "WINS THE SABAAC POT!")
+                winningPlayer.changeChips(self.sabaacPot)
+                self.sabaacPot = 0
+            else:
+                print(winningPlayer.getName(), "wins!")
         pass
     
     def confirmPlayAgain(self):
         print("PLAY AGAIN?")
         remainingPlayers = []
+        playerCount = len(self.playerList)
         
         for player in self.playerList:
             print(player.getName())
@@ -211,6 +289,10 @@ class Game:
             
             if result == 1:
                 remainingPlayers.append(player)
+            if result == 2:
+                playerCount -= 1
+                if playerCount == 1:
+                    break
                 
         self.playerList = remainingPlayers
         pass
@@ -230,6 +312,13 @@ class Game:
     def drawCardForPlayer(self, player):
        nextCard = self.deck.draw()
        player.addToHand(nextCard)
+       
+    def printCurrentGameState(self, player):
+        print(player.getName())
+        print("Chips:", player.getChips())
+        print("Hand Pot:", self.handPot)
+        print("Sabaac Pot:", self.sabaacPot)
+        player.printHand()
     
 def main():
     players = [ "Player1", "Player2", "Player3" ]
@@ -245,6 +334,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-    
-    
